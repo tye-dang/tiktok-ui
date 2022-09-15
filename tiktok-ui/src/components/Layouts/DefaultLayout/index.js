b2eeafee71bfda54a653c5974f59ef71b5c8d8b2
tye-dang <tye@bradshawgroup.global>
2022-09-15 11:52:16 +0700

    Add layout default

--- tiktok-ui/src/components/Layouts/DefaultLayout/index.js
+++ tiktok-ui/src/components/Layouts/DefaultLayout/index.js
@@ -10,9 +10,9 @@ function DefaultLayout({ children }) {
         <div className={cx("wrapper")}>
             <Header />
             <div className={cx("container")}>
+                <Sidebar />
                 <div className={cx("content")}>
-                    <img src="https://titanium.exdomain.net/image/cache/catalog/slider/Gifting_month_HP_D-2-1920x720.jpg" height={"450px"} width={"100%"}/>
-                        {children}
+                    {children}
                 </div>
             </div>
         </div>
--- tiktok-ui/src/components/Layouts/components/Header/index.js
+++ tiktok-ui/src/components/Layouts/components/Header/index.js
@@ -5,19 +5,7 @@ const cx = classNames.bind(styles);
 function Header() {
     return ( 
         <header className={cx("wrapper")}>
-            <div className={cx("inner")}>
-                <h2 style={{color: "#FFFFFF", fontWeight: 500}}>Tye Dang</h2>
-            </div>
-            <div className={cx("menu-layout")}>
-                <ul className={cx("menu-ul")}>
-                    <li className={cx("menu-li")}><a href="#">Home</a></li>
-                    <li className={cx("menu-li")}><a href="#">About</a></li>
-                    <li className={cx("menu-li")}><a href="#">Product</a></li>
-                    <li className={cx("menu-li")}><a href="#">News</a></li>
-                    <li className={cx("menu-li")}><a href="#">Contact</a></li>
-                </ul>
-            </div>
-            
+            <div className={cx("inner")}></div>  
         </header>
      );
 }
